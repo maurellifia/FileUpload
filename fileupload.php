ba8a88fac1180eb068d09cd90b41b374165f5b56
maurellifia <140909596+maurellifia@users.noreply.github.com>
2023-08-13 19:56:10 +0700

    Update fileupload.php

--- fileupload.php
+++ fileupload.php
@@ -1,16 +1,27 @@
 <?php
 
+if($_SERVER['REQUEST_METHOD']== "POST"){
+    $email = $_POST['email'];
+    
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        echo "Invalid Email Format!";
+    }
 
-
-
-
-
+    if(isset($_POST['submit'])){
+        $extension = strtolower(pathinfo($_FILES["Files"]["name"], PATHINFO_EXTENSION));
+        if($extension=='.jpg' || $extension=='.jpeg' || $extension=='.png'){
+            $pdo = new PDO('mysql:host=localhost; dbname=upload', 'root');
+            $stmt = $pdo->prepare("INSERT INTO upload(email, files) VALUES (?, ?)");
+            $stmt->execute([$email, $_FILES["files"]["name"]]); 
+        }
+        else{
+            echo "Please Upload JPG or PNG Only!";
+        }
+    }
+    echo "Upload Successfull!";
+}
 
 ?>
-
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -22,15 +33,14 @@
         <h1> Upload File and Add Email here</h1>
 
         <p>Input your email here</p>
-        <input type="email" name="email"  placeholder="Enter your email" required/>
+        <input type="email" name="email" placeholder="Enter your email" required/>
         <br>
         <br>
         <label>Select file to upload (JPEG or PNG)</label>
-        <input type="file" name="file" accept=".jpeg, .png, .jpg" required />
+        <input type="file" name="files" accept=".jpeg, .png, .jpg" required />
         <br>
         <br>
         <input type="submit" value="Sumbit"/>
     </form>
 </body>
 </html>
-
