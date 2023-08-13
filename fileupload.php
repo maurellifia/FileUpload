9380062223acfb71cacf6424e1ea4a8cb176bf76
maurellifia <140909596+maurellifia@users.noreply.github.com>
2023-08-13 19:08:48 +0700

    Update fileupload.php

--- fileupload.php
+++ fileupload.php
@@ -1,23 +1,35 @@
+<?php
+
+
+
+
+
+
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>UploadFile</title>
 </head>
 <body>
 <form action ="" method="POST" enctype= "multipart/form-data">
         <h1> Upload File and Add Email here</h1>
 
         <p>Input your email here</p>
-        <input type="email" name="email" required>
+        <input type="email" name="email"  placeholder="Enter your email" required/>
         <br>
         <br>
         <label>Select file to upload (JPEG or PNG)</label>
-        <input type="file" name="file" accept="JPEG, PNG" required>
+        <input type="file" name="file" accept=".jpeg, .png, .jpg" required />
         <br>
         <br>
-        <input type="submit" value="Sumbit">
+        <input type="submit" value="Sumbit"/>
     </form>
 </body>
 </html>
