d2cb33172d57817eacb6e5525623ae6c853c3e38
maurellifia <140909596+maurellifia@users.noreply.github.com>
2023-08-13 18:51:52 +0700

    Add files via upload

---
+++ fileupload.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action ="" method="POST" enctype= "multipart/form-data">
+        <h1> Upload File and Add Email here</h1>
+
+        <p>Input your email here</p>
+        <input type="email" name="email" required/>
+        <br>
+        <br>
+        <label>Select file to upload (JPEG or PNG)</label>
+        <input type="file" name="file" accept="JPEG, PNG" required />
+        <br>
+        <br>
+        <input type="submit" value="Sumbit">
+    </form>
+</body>
+</html>
+
